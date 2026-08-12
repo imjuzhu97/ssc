4a1ac3ce65b0d7df6d15d3c075e740c617dad255
imjuzhu97 <imjuzhu97@users.noreply.github.com>
2026-08-12 10:22:23 +0900

    Restore August 15 Luma link

--- events-data.js
+++ events-data.js
@@ -16,9 +16,8 @@ window.SSC_EVENTS = {
       menu: "3-course",
       image: "assets/framer/event-greenpoint-20260815.png?v=20260625-events",
       imageAlt: "August 15 Love at First Spice brunch poster for Mala Project Greenpoint",
-      ctaLabel: "SOLD OUT",
-      ctaUrl: "https://resy.com/cities/new-york-ny/venues/mala-project-greenpoint/events/ma-la-project-x-odd-one-in-present-the-spiciest-supper-club-singles-brunch-edition-2026-08-15",
-      soldOut: true,
+      ctaLabel: "RSVP on Luma",
+      ctaUrl: "https://luma.com/3qd2ff20",
     },
     {
       dateLabel: "August 26",
